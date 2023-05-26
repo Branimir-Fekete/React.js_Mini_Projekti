cf2df5afee633a4d0efbacaf2050cec0a824e732
Branimir-Fekete <branimir.fekete@gmail.com>
2023-05-26 17:26:12 +0200

    Update index.js
    Zakomentirano je zbog vježbe...

--- prva-app/src/index.js
+++ prva-app/src/index.js
@@ -1,10 +1,48 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-
+/*
 function Greeting(){
-    return <h2>My first component</h2>;
+    return <h1>Ovo je naslov</h1>;
 }
+*/
+
+/*
+function Greeting(){
+    return React.createElement('h2',{},'hello world');
+}
+*/
+
+/*
+function Greeting(){
+    return (
+    <div>
+        <h2>My element</h2>
+    </div>);
+}
+*/
+
+/*
+function Greeting() {
+    return(
+    <React.Fragment>
+        <div>
+            <h3>
+                <ul>
+                    <li>
+                        <a href="#">Hello world</a>
+                    </li>
+                </ul>
+            </h3>
+        </div>
+        <h2>Novi naslov</h2>
+    </React.Fragment>
+    );
+}
+*/
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
