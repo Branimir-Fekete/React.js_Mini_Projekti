c5c423044ddc80813e5bcffe796e8e1f63cdff1e
Branimir-Fekete <branimir.fekete@gmail.com>
2023-12-12 16:22:00 +0100

    Update shopping-cart-context.jsx

--- 07Projekt_useContext_useReducer/src/store/shopping-cart-context.jsx
+++ 07Projekt_useContext_useReducer/src/store/shopping-cart-context.jsx
@@ -1,26 +1,31 @@
 import { createContext, useReducer } from 'react';
 
+// Uvoz privremenog niza proizvoda
 import { DUMMY_PRODUCTS } from '../dummy-products';
 
+// Stvaranje novog konteksta za košaricu
 export const CartContext = createContext({
   items: [],
   addItemToCart: () => {},
   updateItemQuantity: () => {},
 });
 
+// Reducer funkcija za upravljanje stanjem košarice
 function shoppingCartReducer(state, action) {
   if (action.type === 'ADD_ITEM') {
-    const updatedItems = [...state.items];
+    const updatedItems = [...state.items]; // Kopija trenutnog stanja stavki
 
+    // Traženje postojeće stavke u košarici
     const existingCartItemIndex = updatedItems.findIndex(
       (cartItem) => cartItem.id === action.payload
     );
     const existingCartItem = updatedItems[existingCartItemIndex];
 
+    // Ažuriranje ili dodavanje nove stavke u košaricu
     if (existingCartItem) {
       const updatedItem = {
         ...existingCartItem,
-        quantity: existingCartItem.quantity + 1,
+        quantity: existingCartItem.quantity + 1, // Povećavanje količine
       };
       updatedItems[existingCartItemIndex] = updatedItem;
     } else {
@@ -40,6 +45,7 @@ function shoppingCartReducer(state, action) {
     };
   }
 
+  // Ažuriranje količine postojeće stavke u košarici
   if (action.type === 'UPDATE_ITEM') {
     const updatedItems = [...state.items];
     const updatedItemIndex = updatedItems.findIndex(
@@ -50,23 +56,28 @@ function shoppingCartReducer(state, action) {
       ...updatedItems[updatedItemIndex],
     };
 
-    updatedItem.quantity += action.payload.amount;
+    updatedItem.quantity += action.payload.amount; // Ažuriranje količine
 
+    // Uklanjanje stavke iz košarice ako je količina 0 ili manja
     if (updatedItem.quantity <= 0) {
       updatedItems.splice(updatedItemIndex, 1);
     } else {
       updatedItems[updatedItemIndex] = updatedItem;
     }
 
+    // Vraćanje ažuriranog stanja košarice
     return {
       ...state,
       items: updatedItems,
     };
   }
+
   return state;
 }
 
+// Komponenta pružatelja konteksta za košaricu
 export default function CartContextProvider({ children }) {
+  // Upotreba useReducer hooka za upravljanje stanjem košarice
   const [shoppingCartState, shoppingCartDispatch] = useReducer(
     shoppingCartReducer,
     {
@@ -74,6 +85,7 @@ export default function CartContextProvider({ children }) {
     }
   );
 
+  // Funkcija za dodavanje stavke u košaricu
   function handleAddItemToCart(id) {
     shoppingCartDispatch({
       type: 'ADD_ITEM',
@@ -81,6 +93,7 @@ export default function CartContextProvider({ children }) {
     });
   }
 
+  // Funkcija za ažuriranje količine stavke u košarici
   function handleUpdateCartItemQuantity(productId, amount) {
     shoppingCartDispatch({
       type: 'UPDATE_ITEM',
@@ -91,12 +104,13 @@ export default function CartContextProvider({ children }) {
     });
   }
 
+  // Priprema vrijednosti za pružanje kroz kontekst
   const ctxValue = {
     items: shoppingCartState.items,
     addItemToCart: handleAddItemToCart,
     updateItemQuantity: handleUpdateCartItemQuantity,
   };
-
+  // Pružanje konteksta potomcima komponente
   return (
     <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
   );
