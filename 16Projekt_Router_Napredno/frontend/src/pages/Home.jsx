4c2b32698bc1ec3a220727d53cf1f3ed2d926660
Branimir-Fekete <branimir.fekete@gmail.com>
2023-12-22 22:33:32 +0100

    Projekti

---
+++ 16Projekt_Router_Napredno/frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import PageContent from '../components/PageContent';
+
+function HomePage() {
+  return (
+    <PageContent title='Welcome'>
+      <p>Pozdrav svijete</p>
+    </PageContent>
+  );
+}
+
+export default HomePage;
---
+++ 16Projekt_Router_Napredno/frontend/src/pages/NewEvent.jsx
@@ -0,0 +1,8 @@
+
+import EventForm from '../components/EventForm';
+
+function NewEventsPage() {
+  return <EventForm method='post' />;
+}
+
+export default NewEventsPage;
